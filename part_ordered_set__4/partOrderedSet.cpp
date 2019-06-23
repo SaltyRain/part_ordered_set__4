d9a8d0971aa878be42844c3282fa03c32fcc4514
Timur Garipov <tbgaripov@gmail.com>
2019-06-23 14:59:14 +0300

    create file structure

---
+++ part_ordered_set__4/partOrderedSet.cpp
@@ -0,0 +1,9 @@
+//
+//  partOrderedSet.cpp
+//  part_ordered_set__4
+//
+//  Created by Тимур Гарипов on 23/06/2019.
+//  Copyright © 2019 Timur. All rights reserved.
+//
+
+#include "partOrderedSet.hpp"
---
+++ part_ordered_set__4/partOrderedSet.hpp
@@ -0,0 +1,14 @@
+//
+//  partOrderedSet.hpp
+//  part_ordered_set__4
+//
+//  Created by Тимур Гарипов on 23/06/2019.
+//  Copyright © 2019 Timur. All rights reserved.
+//
+
+#ifndef partOrderedSet_hpp
+#define partOrderedSet_hpp
+
+#include <stdio.h>
+
+#endif /* partOrderedSet_hpp */
