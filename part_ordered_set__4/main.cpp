54f646646c6d7afa483eb54c490c1c6d5dc853fb
Timur Garipov <tbgaripov@gmail.com>
2019-06-23 13:26:13 +0300

    Initial Commit

---
+++ part_ordered_set__4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  part_ordered_set__4
+//
+//  Created by Тимур Гарипов on 23/06/2019.
+//  Copyright © 2019 Timur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
